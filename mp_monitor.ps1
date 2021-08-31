455ad1753b2c9e0990141308bfa75c019edc8584
overwatch85 <mail@markfsa.com>
2021-08-31 16:46:42 +0200

    Create mp_monitor.ps1
    Powershell loop to monitor a tracked item via Maltapost's API every 2 minutes for real-time updates to a parcel's status

---
+++ mp_monitor.ps1
@@ -0,0 +1,16 @@
+##Title: Maltapost Monitor Looper
+##Author: Mark Farrugia Sant'Angelo
+##Date: 31/08/2021
+##Description: Powershell loop to monitor a tracked item via Maltapost's API every 2 minutes for real-time updates to a parcel's status
+
+$tcode = Read-Host -Prompt 'Input your parcel tracking code'
+
+Function monitor {
+cls
+$response = Invoke-RestMethod -Uri "https://www.maltapost.com/TrackAndTraceApi/v1/trackedItems?barcode=$tcode"
+$response.flag
+echo $response.movements
+}
+
+while (1) {monitor; sleep 120}
+
